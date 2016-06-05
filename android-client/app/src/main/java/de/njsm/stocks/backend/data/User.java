4c493c4f3eb56e1684ac249d2fed09eaaebb473f
f1rst_unicorn <f1rst_unicorn@njsm.de>
2016-06-05 12:06:01 +0200

    Begin developing database handler
    Added methods for user storing

---
+++ android-client/app/src/main/java/de/njsm/stocks/backend/data/User.java
@@ -0,0 +1,12 @@
+package de.njsm.stocks.backend.data;
+
+
+public class User {
+    public int id;
+    public String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
