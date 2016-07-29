3185a05c0484cbb9c3160c7eb5cdf42ca7490008
f1rst_unicorn <f1rst_unicorn@njsm.de>
2016-07-29 16:35:21 +0200

    Lowered minimum SDK version to 14 (4.0)

--- android-client/app/src/main/java/de/njsm/stocks/LocationActivity.java
+++ android-client/app/src/main/java/de/njsm/stocks/LocationActivity.java
@@ -1,51 +1,27 @@
 package de.njsm.stocks;
 
-import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 
-import de.njsm.stocks.backend.data.Food;
-import de.njsm.stocks.backend.db.DatabaseHandler;
-
-public class LocationActivity extends ListActivity {
+public class LocationActivity extends AppCompatActivity {
 
     public static final String KEY_LOCATION_ID = "de.njsm.stocks.LocationActivity.id";
     public static final String KEY_LOCATION_NAME = "de.njsm.stocks.LocationActivity.name";
 
-    Food[] food;
+    protected String mLocation;
+    protected int mId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle(getIntent().getExtras().getString(KEY_LOCATION_NAME));
+        setContentView(R.layout.activity_location);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                food = DatabaseHandler.h.getFood();
-                String[] names = new String[food.length];
-                for (int i = 0; i < food.length; i++){
-                    names[i] = food[i].name;
-                }
+        mLocation = getIntent().getExtras().getString(KEY_LOCATION_NAME);
+        mId = getIntent().getExtras().getInt(KEY_LOCATION_ID);
 
-                final ListAdapter content = new ArrayAdapter<>(LocationActivity.this,
-                        android.R.layout.simple_list_item_1,
-                        names);
+        setTitle(mLocation);
 
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        setListAdapter(content);
-                    }
-                });
-            }
-        }).start();
+
 
 
     }
