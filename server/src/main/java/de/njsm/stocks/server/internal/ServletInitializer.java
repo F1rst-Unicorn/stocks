f8cb3842784bf873806b5013a7109a8f78413b97
f1rst_unicorn <f1rst_unicorn@njsm.de>
2016-11-05 12:25:53 +0100

    server-00016: Add SCI to satisfy tomcat
    Added empty ServletContainerInitializer to both server and sentry to
    allow tomcat deployment

---
+++ server/src/main/java/de/njsm/stocks/server/internal/ServletInitializer.java
@@ -0,0 +1,14 @@
+package de.njsm.stocks.server.internal;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.Set;
+
+public class ServletInitializer implements ServletContainerInitializer {
+
+    @Override
+    public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
+
+    }
+}
