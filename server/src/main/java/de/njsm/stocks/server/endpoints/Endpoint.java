2dbcb1d35947aaa65ce74e0f93d90fe1a27b5ffd
f1rst_unicorn <f1rst_unicorn@njsm.de>
2016-11-06 17:19:45 +0100

    server-00001: Change log4j config provision
    Move log4j to resources to be in class path. Remove ugly workaround to
    set property

--- server/src/main/java/de/njsm/stocks/server/endpoints/Endpoint.java
+++ server/src/main/java/de/njsm/stocks/server/endpoints/Endpoint.java
@@ -8,14 +8,6 @@ class Endpoint {
     final Config c;
     final SqlDatabaseHandler handler;
 
-    // This global initialisation is done here, because this is one
-    // of the first classes loaded by the container
-    static {
-        if (null == System.getProperty("log4j.configurationFile")) {
-            System.getProperties().setProperty("log4j.configurationFile", "/etc/stocks-server/log4j.xml");
-        }
-    }
-
     Endpoint() {
         c = new Config();
         handler = c.getDbHandler();
