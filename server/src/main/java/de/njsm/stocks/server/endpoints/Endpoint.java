9af728af5d3d233aeabf1284a1866ec3af1847d8
f1rst_unicorn <f1rst_unicorn@njsm.de>
2016-12-03 11:06:11 +0100

    server-00021: Introduce access logging
    Add a method for all endpoints to uniformly log all accesses

--- server/src/main/java/de/njsm/stocks/server/endpoints/Endpoint.java
+++ server/src/main/java/de/njsm/stocks/server/endpoints/Endpoint.java
@@ -1,7 +1,11 @@
 package de.njsm.stocks.server.endpoints;
 
 import de.njsm.stocks.server.internal.Config;
+import de.njsm.stocks.server.internal.auth.Principals;
 import de.njsm.stocks.server.internal.db.SqlDatabaseHandler;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
 
 class Endpoint {
 
@@ -11,5 +15,17 @@ class Endpoint {
     Endpoint() {
         c = new Config();
         handler = c.getDbHandler();
+    }
+
+    public void logAccess(Logger targetLog,
+                          HttpServletRequest request,
+                          String description) {
+        Principals userInfo = c.getContextFactory().getPrincipals(request);
+        String logEntry = String.format("%s@%s %s",
+                userInfo.getUsername(),
+                userInfo.getDeviceName(),
+                description);
+
+        targetLog.info(logEntry);
     }
 }
