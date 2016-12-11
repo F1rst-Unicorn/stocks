73f6f9dbfb420ff5255b0b870d2b50f376ccac95
f1rst_unicorn <f1rst_unicorn@njsm.de>
2016-12-11 16:34:29 +0100

    dev-server: Fix wrong table name
    Fix wrong table name in migration script for database

--- deploy-server/db-migration.sql
+++ deploy-server/db-migration.sql
@@ -1,8 +1,8 @@
 SET foreign_key_checks = 0;
     
-DROP TABLE IF EXISTS EAN_Number;
+DROP TABLE IF EXISTS EAN_number;
 
-CREATE TABLE EAN_Number (
+CREATE TABLE EAN_number (
     `ID` int UNSIGNED NOT NULL AUTO_INCREMENT,
     `number` varchar(13) NOT NULL,
     `identifies` int UNSIGNED NOT NULL,
@@ -12,27 +12,27 @@ CREATE TABLE EAN_Number (
 
 INSERT INTO Updates (`table_name`, `last_update`)
 VALUES 
-    ('EAN_Number', NOW());
+    ('EAN_number', NOW());
     
 
 delimiter |    
 
-CREATE TRIGGER EAN_Number_insert
-AFTER INSERT ON EAN_Number FOR EACH ROW
+CREATE TRIGGER EAN_number_insert
+AFTER INSERT ON EAN_number FOR EACH ROW
 BEGIN
-    UPDATE Updates SET last_update=NOW() WHERE `table_name`='EAN_Number';
+    UPDATE Updates SET last_update=NOW() WHERE `table_name`='EAN_number';
 END;
 
-CREATE TRIGGER EAN_Number_update
-AFTER UPDATE ON EAN_Number FOR EACH ROW
+CREATE TRIGGER EAN_number_update
+AFTER UPDATE ON EAN_number FOR EACH ROW
 BEGIN
-    UPDATE Updates SET last_update=NOW() WHERE `table_name`='EAN_Number';
+    UPDATE Updates SET last_update=NOW() WHERE `table_name`='EAN_number';
 END;
 
-CREATE TRIGGER EAN_Number_delete
-AFTER DELETE ON EAN_Number FOR EACH ROW
+CREATE TRIGGER EAN_number_delete
+AFTER DELETE ON EAN_number FOR EACH ROW
 BEGIN
-    UPDATE Updates SET last_update=NOW() WHERE `table_name`='EAN_Number';
+    UPDATE Updates SET last_update=NOW() WHERE `table_name`='EAN_number';
 END;
 
 |
