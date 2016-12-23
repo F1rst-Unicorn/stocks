4621d77ab73decb417653a6340f0d7cb7f8b7433
f1rst_unicorn <f1rst_unicorn@njsm.de>
2016-12-23 14:59:13 +0100

    server-00025: Remove duplicate code
    Move duplicate code into a shared lib file
    Clean directory structure to show purpose of files

---
+++ server/src/test/system/bin/fresh-installation-test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+STOCKS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../../../.."
+RESOURCES=$STOCKS_ROOT/server/src/test/system/tmp/
+
+source $STOCKS_ROOT/server/src/test/system/lib/lib.sh
+
+if [[ $# -ne 1 ]] ; then
+        echo "Usage: $0 <hostname/IP>"
+        exit 1
+fi
+
+SERVER=$1
+
+set -e
+mkdir -p $RESOURCES
+
+createFirstUser $RESOURCES
+checkInitialServer
+checkLocations
+checkUsers
+checkFood
+checkEanNumbers
+checkFoodItems
+checkDevicesAndRevocation
+
+rm -rf $RESOURCES
+
+echo SUCCESS
--- server/src/test/system/bin/vm-deployment-test.sh
+++ server/src/test/system/bin/vm-deployment-test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-STOCKS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../../.."
+STOCKS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../../../.."
 
 # virsh reset
 sudo virsh snapshot-revert dp-server clean-running
@@ -11,7 +11,7 @@ sleep 1
 ansible-playbook $STOCKS_ROOT/deploy-server/install.yml
 ansible-playbook $STOCKS_ROOT/deploy-server/deploy.yml
 
-$STOCKS_ROOT/server/src/test/system/system-test.sh dp-server
+$STOCKS_ROOT/server/src/test/system/bin/fresh-installation-test.sh dp-server
 
 sudo virsh snapshot-revert dp-server clean
 
