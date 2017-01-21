f665c8ae7236b0a0b19fa01ee1effd66c4e7e256
f1rst_unicorn <f1rst_unicorn@njsm.de>
2017-01-21 16:33:15 +0100

    client-00008: Add packaging script
    Add script to package the client from within the repository.
    Adapt PKGBUILD accordingly.

---
+++ deploy-client/bin/package.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set -x
+
+STOCKS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
+CWD=$(pwd)
+
+mkdir -p $STOCKS_ROOT/deploy-client/target
+
+tar -cvf $STOCKS_ROOT/deploy-client/stocks.tar \
+        --exclude=android-client \
+        --exclude=target \
+        --exclude=deploy-server \
+        $STOCKS_ROOT
+
+cd $STOCKS_ROOT/deploy-client
+makepkg -fc
+cd $CWD
+
+gpg --detach-sign --use-agent $STOCKS_ROOT/deploy-client/*.xz
+
+mv $STOCKS_ROOT/deploy-client/*.sig $STOCKS_ROOT/deploy-client/target
+mv $STOCKS_ROOT/deploy-client/*.xz $STOCKS_ROOT/deploy-client/target
+rm -rf $STOCKS_ROOT/deploy-client/src \
+        $STOCKS_ROOT/deploy-client/pkg \
+        $STOCKS_ROOT/deploy-client/*.tar
+
