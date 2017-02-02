cfff1808bc2fc7bbdffbbc8167fa60559d50925c
f1rst_unicorn <f1rst_unicorn@njsm.de>
2017-02-02 21:08:58 +0100

    client-00009: Using JUnitParameterized
    Using library instead of for loop for testing

--- client/src/test/java/de/njsm/stocks/client/network/TcpHostTest.java
+++ client/src/test/java/de/njsm/stocks/client/network/TcpHostTest.java
@@ -1,25 +1,54 @@
 package de.njsm.stocks.client.network;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.*;
 
+@RunWith(JUnitParamsRunner.class)
 public class TcpHostTest {
 
     @Test
-    public void testValidParameters() {
-        for (int portToTest = 1; portToTest < 65536; portToTest++) {
-            assertTrue("Port " + portToTest, TcpHost.isValidPort(portToTest));
-        }
+    @Parameters(method = "getValidPorts")
+    public void testValidParameters(int portToTest) {
+        assertTrue("Port " + portToTest, TcpHost.isValidPort(portToTest));
+    }
+
+    public Object[] getValidPorts() {
+        return new Object[] {
+                new Object[] {1},
+                new Object[] {2},
+                new Object[] {3},
+                new Object[] {22},
+                new Object[] {80},
+                new Object[] {443},
+                new Object[] {10910},
+                new Object[] {10911},
+                new Object[] {10912}
+        };
     }
 
     @Test
-    public void testInvalidParameters() {
-        assertFalse("Port 0", TcpHost.isValidPort(0));
-        for (int portToTest = 65536; portToTest < 100000; portToTest++) {
-            assertFalse("Port " + portToTest, TcpHost.isValidPort(portToTest));
-        }
+    @Parameters(method = "getInvalidPorts")
+    public void testInvalidParameters(int portToTest) {
+        assertFalse("Port " + portToTest, TcpHost.isValidPort(portToTest));
+    }
+
+    public Object[] getInvalidPorts() {
+        return new Object[] {
+                new Object[] {0},
+                new Object[] {-1},
+                new Object[] {-10},
+                new Object[] {-100},
+                new Object[] {Integer.MIN_VALUE},
+                new Object[] {65536},
+                new Object[] {65537},
+                new Object[] {65538},
+                new Object[] {Integer.MAX_VALUE}
+        };
     }
 
     @Test
