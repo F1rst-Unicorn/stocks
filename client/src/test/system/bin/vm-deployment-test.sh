b7a308ee853e6d90eeb8b4a2144d8c8c687ead09
f1rst_unicorn <f1rst_unicorn@njsm.de>
2017-02-02 21:10:14 +0100

    client-00009: Fix hardcoded fingerprint
    Remove fingerprint from system test and replace by reading it from the
    server cert

--- client/src/test/system/bin/vm-deployment-test.sh
+++ client/src/test/system/bin/vm-deployment-test.sh
@@ -12,9 +12,13 @@ rm -rf $STOCKS_ROOT/client/src/test/system/tmp
 mkdir -p $STOCKS_ROOT/client/src/test/system/tmp/.stocks
 cp $STOCKS_ROOT/client/src/test/system/{stocks.db,tmp/.stocks/}
 
+FINGERPRINT=$(curl -s http://dp-client-server:10910/ca | \
+        openssl x509 -noout -sha256 -fingerprint | \
+        head -n 1 | sed 's/.*=//')
+
 echo -e "dp-client-server\n\n\n\nJack\nDevice\n1\n1\n\
-7F:40:EB:D6:91:A5:84:62:D4:48:5C:41:7A:7A:C8:FC:03:EB:92:C3:F3:F5:C7:39:9E:00:BC:3C:7D:FA:47:E8\n\
-0000\nrefresh\nuser\ndevices\nfood\nlocation\nquit\n" \
+$FINGERPRINT\n\
+0000\nrefresh\nuser\ndev\nfood\nloc\nquit\n" \
         | java -jar -Duser.stocks.dir=$STOCKS_ROOT/client/src/test/system/tmp \
         $STOCKS_ROOT/client/target/client-*-jar-with-dependencies.jar
 
