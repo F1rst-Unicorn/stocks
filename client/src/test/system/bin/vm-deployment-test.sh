d666130a3edf13c210c13029ed9436d1f5a34238
f1rst_unicorn <f1rst_unicorn@njsm.de>
2017-03-22 17:28:49 +0100

    client-00009: Remove jacoco coverage from system test

--- client/src/test/system/bin/vm-deployment-test.sh
+++ client/src/test/system/bin/vm-deployment-test.sh
@@ -28,8 +28,6 @@ $FINGERPRINT\n\
 0000\nrefresh\nuser\ndev\nfood\nloc\nquit\n" \
         | java -jar -Duser.stocks.dir=$STOCKS_ROOT/client/src/test/system/tmp \
         -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9999 \
-        -javaagent:/opt/jacoco/lib/jacocoagent.jar=destfile=/tmp/jacoco-\
-client.exec,dumponexit=true,output=file\
         $STOCKS_ROOT/client/target/client-*.jar
 
 rm -rf $STOCKS_ROOT/client/src/test/system/tmp
