58a7b8b650be9670692af2d11198d80c5ca3857d
f1rst_unicorn <f1rst_unicorn@njsm.de>
2017-09-22 12:04:44 +0200

    STOCKS-25: Add search submit button
    Add button to submit search query
    Remove comments

--- android-client/app/src/main/java/de/njsm/stocks/frontend/main/MainActivity.java
+++ android-client/app/src/main/java/de/njsm/stocks/frontend/main/MainActivity.java
@@ -126,12 +126,11 @@ public class MainActivity extends AppCompatActivity
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
 
-        // Get the SearchView and set the searchable configuration
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        // Assumes current activity is the searchable activity
         searchView.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(this, SearchActivity.class)));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+        searchView.setIconifiedByDefault(true);
+        searchView.setSubmitButtonEnabled(true);
         return true;
     }
 
