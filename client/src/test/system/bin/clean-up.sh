847d688b6fcb2daa73fe4e7b047bf450fad7284f
f1rst_unicorn <f1rst_unicorn@njsm.de>
2018-01-23 11:54:53 +0100

    STOCKS-10: Make clean up scripts idempotent
    Don't fail if the server has already been cleaned

--- client/src/test/system/bin/clean-up.sh
+++ client/src/test/system/bin/clean-up.sh
@@ -2,3 +2,5 @@
 
 ssh dp-client sudo pacman -Rsn stocks --noconfirm
 ssh dp-client rm -rf \~/.stocks
+
+exit 0
--- server/src/test/system/bin/clean-up.sh
+++ server/src/test/system/bin/clean-up.sh
@@ -2,3 +2,5 @@
 
 ssh dp-server sudo systemctl stop tomcat8
 ssh dp-server sudo pacman -Rsn stocks-server --noconfirm
+
+exit 0
