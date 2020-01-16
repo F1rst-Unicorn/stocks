88b62b2b377c17d839d5d94af22b8cef2aa62c08
Jan Veen <jan.veen@posteo.de>
2020-01-16 14:23:05 +0100

    Increment android client version to 3.3.0.1

--- android-client/app/src/main/java/de/njsm/stocks/android/db/StocksDatabase.java
+++ android-client/app/src/main/java/de/njsm/stocks/android/db/StocksDatabase.java
@@ -33,7 +33,7 @@ import de.njsm.stocks.android.db.entities.*;
         Food.class,
         FoodItem.class,
         EanNumber.class,
-}, version = 25)
+}, version = 26)
 @TypeConverters(de.njsm.stocks.android.db.TypeConverters.class)
 public abstract class StocksDatabase extends RoomDatabase {
 
