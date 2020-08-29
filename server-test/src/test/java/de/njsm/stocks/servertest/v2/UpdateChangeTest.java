dca1f0996a69abc7ff75f409b475b39d912b088c
Jan Veen <jan.veen@posteo.de>
2020-08-29 09:49:03 +0200

    STOCKS-101 Add system tests
    Make sure the update-change test checks for changed dates. As long as
    the table is empty, the update-change date is alwas now(). Fix the
    initial change date by having a non-empty table.
    
    Add a test to query for changed data. Make sure when requesting only
    changed data that the server replies with only relevant changes

--- server-test/src/test/java/de/njsm/stocks/servertest/v2/UpdateChangeTest.java
+++ server-test/src/test/java/de/njsm/stocks/servertest/v2/UpdateChangeTest.java
@@ -22,13 +22,14 @@ package de.njsm.stocks.servertest.v2;
 import de.njsm.stocks.servertest.TestSuite;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.iterableWithSize;
-import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.hamcrest.core.IsIterableContaining.hasItem;
 import static org.junit.Assert.assertTrue;
 
 public class UpdateChangeTest {
@@ -36,14 +37,43 @@ public class UpdateChangeTest {
     @Test
     public void updatesChangeOnDataChange() {
         String olderDate = getLocationChangeDate();
-        changeLocations();
+        addALocation();
         String youngerDate = getLocationChangeDate();
+
+        assertTrue(olderDate + " is not older than " + youngerDate,
+                olderDate.compareTo(youngerDate) < 0);
+
+        olderDate = youngerDate;
+        addALocation();
+        youngerDate = getLocationChangeDate();
 
         assertTrue(olderDate + " is not older than " + youngerDate,
                 olderDate.compareTo(youngerDate) < 0);
     }
 
-    private void changeLocations() {
+    @Test
+    public void gettingChangesStartingFromDateWorks() {
+        addALocation();
+        String lastChangeDate = getLocationChangeDate();
+        addALocation();
+
+        getDataYoungerThan(lastChangeDate)
+                .body("data", iterableWithSize(1));
+    }
+
+    private ValidatableResponse getDataYoungerThan(String date) {
+        return given()
+                .queryParam("bitemporal", 1)
+                .queryParam("startingFrom", date).
+        when()
+                .get(TestSuite.DOMAIN + "/v2/location").
+        then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("status", equalTo(0));
+    }
+
+    private void addALocation() {
         given()
                 .queryParam("name", "update").
         when()
