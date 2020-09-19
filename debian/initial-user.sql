e67beea5dcf730c0e08e9b5d9514f15135c25c4d
Jan Veen <jan.veen@posteo.de>
2020-09-19 21:20:33 +0200

    STOCKS-109: Add DB migration
    Extend the DB model to store the creating user and device for every
    record. To allow migration the existing devices' timestamps have to be
    adjusted to model the instant creation of the first user and device
    within the same transaction.
    
    Increase the timeout for checked out connections because these
    migrations are expensive.

--- debian/initial-user.sql
+++ debian/initial-user.sql
@@ -1,4 +1,20 @@
 -- You have to create an initial user for your database.
-INSERT INTO "User" (name) VALUES ('<username>');
-INSERT INTO "User_device" (name, belongs_to) VALUES ('<device>', LASTVAL());
-INSERT INTO "Ticket" (ticket, belongs_device) VALUES ('0000', LASTVAL())
+begin transaction isolation level serializable;
+
+insert into "User" (name, creator_user, creator_user_device)
+values ('<username>', 0, 0);
+
+insert into "User_device" (name, belongs_to, creator_user, creator_user_device)
+values ('<device>', lastval(), 0, 0);
+
+update "User"
+set creator_user = (select min("ID") from "User"),
+creator_user_device = (select min("ID") from "User_device");
+
+update "User_device"
+set creator_user = (select min("ID") from "User"),
+creator_user_device = (select min("ID") from "User_device");
+
+insert into "Ticket" (ticket, belongs_device) values ('0000', lastval());
+
+commit;
