cfd08096f8c1b73df5835dc0517724071ccfa27d
Jan Veen <jan.veen@posteo.de>
2020-09-19 14:04:25 +0200

    STOCKS-103: Fix android system test

--- android-client/app/src/androidTest/java/de/njsm/stocks/screen/OutlineScreen.java
+++ android-client/app/src/androidTest/java/de/njsm/stocks/screen/OutlineScreen.java
@@ -20,16 +20,23 @@
 package de.njsm.stocks.screen;
 
 import android.view.Gravity;
+
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+
 import de.njsm.stocks.R;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.DrawerActions.open;
 import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
-import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 public class OutlineScreen extends AbstractScreen {
@@ -50,6 +57,8 @@ public class OutlineScreen extends AbstractScreen {
     }
 
     public EmptyFoodScreen goToEmptyFood() {
+        onView(withId(R.id.template_swipe_list_list))
+                .perform(RecyclerViewActions.scrollToPosition(0));
         onView(withId(R.id.fragment_outline_header_cardview2)).perform(click());
         return new EmptyFoodScreen();
     }
@@ -79,6 +88,8 @@ public class OutlineScreen extends AbstractScreen {
     }
 
     public EatSoonScreen goToEatSoon() {
+        onView(withId(R.id.template_swipe_list_list))
+                .perform(RecyclerViewActions.scrollToPosition(0));
         onView(withId(R.id.fragment_outline_header_cardview)).perform(click());
         return new EatSoonScreen();
     }
