febe57f57238f1c045aedc42408f9f0d529b51db
Jan Veen <jan.veen@posteo.de>
2020-09-20 18:09:42 +0200

    STOCKS-109: Fix db init scripts

--- debian/initial-user.sql
+++ debian/initial-user.sql
@@ -1,19 +1,19 @@
 -- You have to create an initial user for your database.
 begin transaction isolation level serializable;
 
-insert into "User" (name, creator_user, creator_user_device)
+insert into "User" (name, initiates)
 values ('<username>', 0, 0);
 
-insert into "User_device" (name, belongs_to, creator_user, creator_user_device)
+insert into "User_device" (name, belongs_to, initiates)
 values ('<device>', lastval(), 0, 0);
 
 update "User"
 set creator_user = (select min("ID") from "User"),
-creator_user_device = (select min("ID") from "User_device");
+initiates = (select min("ID") from "User_device");
 
 update "User_device"
 set creator_user = (select min("ID") from "User"),
-creator_user_device = (select min("ID") from "User_device");
+initiates = (select min("ID") from "User_device");
 
 insert into "Ticket" (ticket, belongs_device) values ('0000', lastval());
 
