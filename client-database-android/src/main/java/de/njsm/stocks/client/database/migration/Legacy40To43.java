0f0d31ba01b39b9f10e32ffe1751afeb080896e9
Jan Veen <jan.veen@posteo.de>
2022-12-11 11:09:50 +0100

    Issue #42 Add more migrations

--- client-database-android/src/main/java/de/njsm/stocks/client/database/migration/Legacy40To43.java
+++ client-database-android/src/main/java/de/njsm/stocks/client/database/migration/Legacy40To43.java
@@ -25,6 +25,11 @@ import androidx.annotation.NonNull;
 import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Legacy40To43 extends Migration {
 
     public Legacy40To43() {
@@ -41,11 +46,19 @@ public class Legacy40To43 extends Migration {
     }
 
     private static void handleMainEntityTables(DdlPrimitives ddlPrimitives) {
-        handleEanNumber(ddlPrimitives);
-        handleUserDevice(ddlPrimitives);
-        handleUser(ddlPrimitives);
+        ddlPrimitives.dropView("current_scaled_amount");
+        ddlPrimitives.dropView("current_scaled_unit_conversion");
+        ddlPrimitives.dropView("current_scaled_ingredient_amount_and_stock");
+        ddlPrimitives.dropView("recipe_stock_rating_base");
+        handleUnit(ddlPrimitives);
         handleScaledUnit(ddlPrimitives);
+        handleFoodItem(ddlPrimitives);
+        handleEanNumber(ddlPrimitives);
+        handleUser(ddlPrimitives);
+        handleUserDevice(ddlPrimitives);
+        handleRecipe(ddlPrimitives);
         handleRecipeProduct(ddlPrimitives);
+        handleRecipeIngredient(ddlPrimitives);
     }
 
     private static void handleFailedOperationRecordingTables(DdlPrimitives ddlPrimitives) {
@@ -82,6 +95,12 @@ public class Legacy40To43 extends Migration {
                 "user_id INTEGER",
                 "user_transaction_time TEXT"
         );
+        ddlPrimitives.createTable("location_to_delete", "id",
+                "id INTEGER not null",
+                "version INTEGER not null",
+                "location_id INTEGER",
+                "location_transaction_time TEXT"
+        );
         ddlPrimitives.createTable("food_item_to_edit", "id",
                 "food_item_id INTEGER",
                 "id INTEGER not null",
@@ -103,6 +122,15 @@ public class Legacy40To43 extends Migration {
                 "scaled_unit_id INTEGER",
                 "scaled_unit_transaction_time TEXT",
                 "scale TEXT not null"
+        );
+        ddlPrimitives.createTable("location_to_edit", "id",
+                "id INTEGER not null",
+                "execution_time TEXT",
+                "version INTEGER not null",
+                "location_id INTEGER",
+                "location_transaction_time TEXT",
+                "name TEXT not null",
+                "description TEXT not null"
         );
         ddlPrimitives.createTable("food_to_buy", "id",
                 "id INTEGER not null",
@@ -133,136 +161,123 @@ public class Legacy40To43 extends Migration {
     }
 
     private static void handleEanNumber(DdlPrimitives ddlPrimitives) {
-        ddlPrimitives.createTable("ean_number", "id, version, transaction_time_start",
-                "id INTEGER not null",
-                "version INTEGER not null",
-                "valid_time_start TEXT not null",
-                "valid_time_end TEXT not null",
-                "transaction_time_start TEXT not null",
-                "transaction_time_end TEXT not null",
-                "initiates INTEGER not null",
-                "number TEXT not null",
-                "identifies INTEGER not null"
-        );
-        ddlPrimitives.copyTableContent("eannumber", "ean_number",
-                "_id", "id",
-                "version", "version",
-                "valid_time_start", "valid_time_start",
-                "valid_time_end", "valid_time_end",
-                "transaction_time_start", "transaction_time_start",
-                "transaction_time_end", "transaction_time_end",
-                "initiates", "initiates",
-                "number", "number",
-                "identifies", "identifies"
-        );
-        ddlPrimitives.dropView("current_eannumber");
-        ddlPrimitives.dropTable("eannumber");
-        ddlPrimitives.createIndex("ean_number", "ean_number_current", "1 = 1", "id", "valid_time_start", "valid_time_end");
-        ddlPrimitives.createIndex("ean_number", "ean_number_pkey", "1 = 1", "id");
-        ddlPrimitives.createIndex("ean_number", "ean_number_transaction_time_start", "1 = 1", "transaction_time_start");
-        ddlPrimitives.createIndex("ean_number", "ean_number_transaction_time_end", "1 = 1", "transaction_time_end");
+        copyEntityTable(ddlPrimitives, "eannumber", "ean_number",
+                List.of(
+                        "number TEXT not null",
+                        "identifies INTEGER not null"
+                ), List.of(
+                        "number",
+                        "identifies"
+                ));
     }
 
     private static void handleUserDevice(DdlPrimitives ddlPrimitives) {
-        ddlPrimitives.createTable("user_device_new", "id, version, transaction_time_start",
-                "id INTEGER not null",
-                "version INTEGER not null",
-                "valid_time_start TEXT not null",
-                "valid_time_end TEXT not null",
-                "transaction_time_start TEXT not null",
-                "transaction_time_end TEXT not null",
-                "initiates INTEGER not null",
-                "name TEXT not null",
-                "belongs_to INTEGER not null"
-        );
-        ddlPrimitives.copyTableContent("user_device", "user_device_new",
-                "_id", "id",
-                "version", "version",
-                "valid_time_start", "valid_time_start",
-                "valid_time_end", "valid_time_end",
-                "transaction_time_start", "transaction_time_start",
-                "transaction_time_end", "transaction_time_end",
-                "initiates", "initiates",
-                "name", "name",
-                "belongs_to", "belongs_to"
-        );
-        ddlPrimitives.dropView("current_user_device");
-        ddlPrimitives.dropTable("user_device");
-        ddlPrimitives.renameTable("user_device_new", "user_device");
-        ddlPrimitives.createIndex("user_device", "user_device_current", "1 = 1", "id", "valid_time_start", "valid_time_end");
-        ddlPrimitives.createIndex("user_device", "user_device_pkey", "1 = 1", "id");
-        ddlPrimitives.createIndex("user_device", "user_device_transaction_time_start", "1 = 1", "transaction_time_start");
-        ddlPrimitives.createIndex("user_device", "user_device_transaction_time_end", "1 = 1", "transaction_time_end");
+        copyEntityTable(ddlPrimitives, "user_device", "user_device",
+                List.of(
+                        "name TEXT not null",
+                        "belongs_to INTEGER not null"
+                ), List.of(
+                        "name",
+                        "belongs_to"
+                ));
     }
 
     private static void handleUser(DdlPrimitives ddlPrimitives) {
-        ddlPrimitives.createTable("user_new", "id, version, transaction_time_start",
-                "id INTEGER not null",
-                "version INTEGER not null",
-                "valid_time_start TEXT not null",
-                "valid_time_end TEXT not null",
-                "transaction_time_start TEXT not null",
-                "transaction_time_end TEXT not null",
-                "initiates INTEGER not null",
-                "name TEXT not null"
-        );
-        ddlPrimitives.copyTableContent("user", "user_new",
-                "_id", "id",
-                "version", "version",
-                "valid_time_start", "valid_time_start",
-                "valid_time_end", "valid_time_end",
-                "transaction_time_start", "transaction_time_start",
-                "transaction_time_end", "transaction_time_end",
-                "initiates", "initiates",
-                "name", "name"
-        );
-        ddlPrimitives.dropView("current_user");
-        ddlPrimitives.dropTable("user");
-        ddlPrimitives.renameTable("user_new", "user");
-        ddlPrimitives.createIndex("user", "user_current", "1 = 1", "id", "valid_time_start", "valid_time_end");
-        ddlPrimitives.createIndex("user", "user_pkey", "1 = 1", "id");
-        ddlPrimitives.createIndex("user", "user_transaction_time_start", "1 = 1", "transaction_time_start");
-        ddlPrimitives.createIndex("user", "user_transaction_time_end", "1 = 1", "transaction_time_end");
+        copyEntityTable(ddlPrimitives, "user", "user",
+                List.of(
+                        "name TEXT not null"
+                ), List.of(
+                        "name"
+                ));
+    }
+
+    private static void handleUnit(DdlPrimitives ddlPrimitives) {
+        copyEntityTable(ddlPrimitives, "unit", "unit",
+                List.of(
+                        "name TEXT not null",
+                        "abbreviation TEXT not null"
+                ), List.of(
+                        "name",
+                        "abbreviation"
+                ));
     }
 
     private static void handleScaledUnit(DdlPrimitives ddlPrimitives) {
-        ddlPrimitives.createTable("scaled_unit_new", "id, version, transaction_time_start",
-                "id INTEGER not null",
-                "version INTEGER not null",
-                "valid_time_start TEXT not null",
-                "valid_time_end TEXT not null",
-                "transaction_time_start TEXT not null",
-                "transaction_time_end TEXT not null",
-                "initiates INTEGER not null",
-                "scale TEXT not null",
-                "unit INTEGER not null"
-        );
-        ddlPrimitives.copyTableContent("scaled_unit", "scaled_unit_new",
-                "_id", "id",
-                "version", "version",
-                "valid_time_start", "valid_time_start",
-                "valid_time_end", "valid_time_end",
-                "transaction_time_start", "transaction_time_start",
-                "transaction_time_end", "transaction_time_end",
-                "initiates", "initiates",
-                "scale", "scale",
-                "unit", "unit"
-        );
-        ddlPrimitives.dropView("current_scaled_unit");
-        ddlPrimitives.dropView("current_scaled_amount");
-        ddlPrimitives.dropView("current_scaled_unit_conversion");
-        ddlPrimitives.dropView("current_scaled_ingredient_amount_and_stock");
-        ddlPrimitives.dropView("recipe_stock_rating_base");
-        ddlPrimitives.dropTable("scaled_unit");
-        ddlPrimitives.renameTable("scaled_unit_new", "scaled_unit");
-        ddlPrimitives.createIndex("scaled_unit", "scaled_unit_current", "1 = 1", "id", "valid_time_start", "valid_time_end");
-        ddlPrimitives.createIndex("scaled_unit", "scaled_unit_pkey", "1 = 1", "id");
-        ddlPrimitives.createIndex("scaled_unit", "scaled_unit_transaction_time_start", "1 = 1", "transaction_time_start");
-        ddlPrimitives.createIndex("scaled_unit", "scaled_unit_transaction_time_end", "1 = 1", "transaction_time_end");
+        copyEntityTable(ddlPrimitives, "scaled_unit", "scaled_unit",
+                List.of(
+                        "scale TEXT not null",
+                        "unit INTEGER not null"
+                ), List.of(
+                        "scale",
+                        "unit"
+                ));
     }
 
     private static void handleRecipeProduct(DdlPrimitives ddlPrimitives) {
-        ddlPrimitives.createTable("recipe_product_new", "id, version, transaction_time_start",
+        copyEntityTable(ddlPrimitives, "recipe_product", "recipe_product",
+                List.of(
+                        "recipe INTEGER not null",
+                        "product INTEGER not null",
+                        "unit INTEGER not null",
+                        "amount INTEGER not null"
+                ), List.of(
+                        "recipe",
+                        "product",
+                        "unit",
+                        "amount"
+                ));
+    }
+
+    private static void handleRecipeIngredient(DdlPrimitives ddlPrimitives) {
+        copyEntityTable(ddlPrimitives, "recipe_ingredient", "recipe_ingredient",
+                List.of(
+                        "recipe INTEGER not null",
+                        "ingredient INTEGER not null",
+                        "unit INTEGER not null",
+                        "amount INTEGER not null"
+                ), List.of(
+                        "recipe",
+                        "ingredient",
+                        "unit",
+                        "amount"
+                ));
+    }
+
+    private static void handleRecipe(DdlPrimitives ddlPrimitives) {
+        copyEntityTable(ddlPrimitives, "recipe", "recipe",
+                List.of(
+                        "name TEXT not null",
+                        "duration TEXT not null",
+                        "instructions TEXT not null"
+                ), List.of(
+                        "name",
+                        "duration",
+                        "instructions"
+                ));
+    }
+
+    private static void handleFoodItem(DdlPrimitives ddlPrimitives) {
+        copyEntityTable(ddlPrimitives, "fooditem", "food_item",
+                List.of(
+                        "of_type INTEGER not null",
+                        "stored_in INTEGER not null",
+                        "buys INTEGER not null",
+                        "registers INTEGER not null",
+                        "unit INTEGER not null",
+                        "eat_by TEXT not null"
+                ), List.of(
+                        "of_type",
+                        "stored_in",
+                        "buys",
+                        "registers",
+                        "unit",
+                        "eat_by"
+                ));
+    }
+
+    private static void copyEntityTable(DdlPrimitives ddlPrimitives, String oldName, String newName, List<String> columnsDdl, List<String> columns) {
+        String temporaryName = newName + "_new";
+        String[] allColumnDdls = Stream.concat(Arrays.stream(new String[] {
                 "id INTEGER not null",
                 "version INTEGER not null",
                 "valid_time_start TEXT not null",
@@ -270,12 +285,14 @@ public class Legacy40To43 extends Migration {
                 "transaction_time_start TEXT not null",
                 "transaction_time_end TEXT not null",
                 "initiates INTEGER not null",
-                "recipe INTEGER not null",
-                "product INTEGER not null",
-                "unit INTEGER not null",
-                "amount INTEGER not null"
-        );
-        ddlPrimitives.copyTableContent("recipe_product", "recipe_product_new",
+
+        }), columnsDdl.stream()).toArray(String[]::new);
+        List<String> allCustomColumsDuplicate = new ArrayList<>();
+        columns.forEach(v -> {
+            allCustomColumsDuplicate.add(v);
+            allCustomColumsDuplicate.add(v);
+        });
+        String[] allColumns = Stream.concat(Arrays.stream(new String[] {
                 "_id", "id",
                 "version", "version",
                 "valid_time_start", "valid_time_start",
@@ -283,18 +300,18 @@ public class Legacy40To43 extends Migration {
                 "transaction_time_start", "transaction_time_start",
                 "transaction_time_end", "transaction_time_end",
                 "initiates", "initiates",
-                "recipe", "recipe",
-                "product", "product",
-                "unit", "unit",
-                "amount", "amount"
-        );
-        ddlPrimitives.dropView("current_recipe_product");
-        ddlPrimitives.dropTable("recipe_product");
-        ddlPrimitives.renameTable("recipe_product_new", "recipe_product");
-        ddlPrimitives.createIndex("recipe_product", "recipe_product_current", "1 = 1", "id", "valid_time_start", "valid_time_end");
-        ddlPrimitives.createIndex("recipe_product", "recipe_product_pkey", "1 = 1", "id");
-        ddlPrimitives.createIndex("recipe_product", "recipe_product_transaction_time_start", "1 = 1", "transaction_time_start");
-        ddlPrimitives.createIndex("recipe_product", "recipe_product_transaction_time_end", "1 = 1", "transaction_time_end");
+
+        }), allCustomColumsDuplicate.stream()).toArray(String[]::new);
+
+        ddlPrimitives.createTable(temporaryName, "id, version, transaction_time_start", allColumnDdls);
+        ddlPrimitives.copyTableContent(oldName, temporaryName, allColumns);
+        ddlPrimitives.dropView("current_" + oldName);
+        ddlPrimitives.dropTable(oldName);
+        ddlPrimitives.renameTable(temporaryName, newName);
+        ddlPrimitives.createIndex(newName, newName + "_current", "1 = 1", "id", "valid_time_start", "valid_time_end");
+        ddlPrimitives.createIndex(newName, newName + "_pkey", "1 = 1", "id");
+        ddlPrimitives.createIndex(newName, newName + "_transaction_time_start", "1 = 1", "transaction_time_start");
+        ddlPrimitives.createIndex(newName, newName + "_transaction_time_end", "1 = 1", "transaction_time_end");
     }
 
     private static void handleSearchTables(DdlPrimitives ddlPrimitives) {
